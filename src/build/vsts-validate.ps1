f881ff1be6823605aebec09fa8a669fa6bb4cf3a
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2023-09-20 13:58:34 +0200

    Fix issue

--- src/build/vsts-validate.ps1
+++ src/build/vsts-validate.ps1
@@ -4,4 +4,4 @@
 # Needs to ensure things are Done Right and only legal commits to master get built
 
 # Run internal pester tests
-& "$PSScriptRoot\..\PSOffice365Reports\tests\pester.ps1"
+& "$PSScriptRoot\..\PSMicrosoftReport\tests\pester.ps1"
