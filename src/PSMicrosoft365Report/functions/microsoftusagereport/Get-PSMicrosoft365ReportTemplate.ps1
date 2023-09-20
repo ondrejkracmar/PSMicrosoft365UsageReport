16cd04880be6c70471e6c8c322b02bf5f7b6ef9a
Ondrej Kracmar <ondrej.kracmar@i-system.cz>
2023-09-20 14:37:14 +0200

    Fix issue

--- src/PSMicrosoft365Report/functions/microsoftusagereport/Get-PSMicrosoft365ReportTemplate.ps1
+++ src/PSMicrosoft365Report/functions/microsoftusagereport/Get-PSMicrosoft365ReportTemplate.ps1
@@ -1,15 +1,15 @@
 ﻿function Get-PSMicrosoft365ReportTemplate {
     [OutputType('PSMicrosoft365Report.UsageReport.Template')]
-    [CmdletBinding()]    
+    [CmdletBinding()]
     param()
- 
+
     begin {
         $templateReportList = [System.Collections.ArrayList]::new()
     }
     process {
         $templateReportFileList = Get-PSFConfigValue -FullName ('{0}.Template.Microsoft365.Location' -f $Script:ModuleName) | Get-ChildItem -Filter *.json -Recurse
         foreach ($templateReportFile in $templateReportFileList) {
-            $templateReport = Get-Content -Path $templateReportFile.FullName | ConvertFrom-Json            
+            $templateReport = Get-Content -Path $templateReportFile.FullName | ConvertFrom-Json
             [void]($templateReportList.Add($templateReport))
         }
         $templateReportList
--- src/PSMicrosoft365Report/internal/tepp/microsoft365usagereport.tepp.ps1
+++ src/PSMicrosoft365Report/internal/tepp/microsoft365usagereport.tepp.ps1
@@ -1,4 +1,4 @@
-Register-PSFTeppScriptblock -Name 'microsoft365usagereport.name' -ScriptBlock { (Get-PSMicrosoft365ReportTemplate | Select-Object -Property Name).Name }
+﻿Register-PSFTeppScriptblock -Name 'microsoft365usagereport.name' -ScriptBlock { (Get-PSMicrosoft365ReportTemplate | Select-Object -Property Name).Name }
 Register-PSFTeppScriptblock -Name 'microsoft365usagereport.name.parametertype' -ScriptBlock { ((Get-PSMicrosoft365ReportTemplate | Where-Object -Property Name -EQ $fakeBoundParameter.Name | Select-Object -ExpandProperty Definition).Parameters | Get-Member -MemberType Properties).Name }
 Register-PSFTeppScriptblock -Name 'microsoft365usagereport.name.parametervalue' -ScriptBlock {
 
